30bc7c152de33935da266529942b2d03670b60f2
alexey4petrov <alexey4petrov@yandex.ru>
2010-02-24 15:34:33 +0000

    To improve handling of "--enable-embeddedcxx" configuration option

--- Foam/applications/utilities/emb_createPhi.cxx
+++ Foam/applications/utilities/emb_createPhi.cxx
@@ -1,18 +1,41 @@
+//  Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  See https://csrcs.pbmr.co.za/svn/nea/prototypes/reaktor/pyfoam
+//
+//  Author : Alexey PETROV
+
+
 //---------------------------------------------------------------------------
-#ifndef createPhi_cxx
-#define createPhi_cxx
+#ifndef emb_createPhi_cxx
+#define emb_createPhi_cxx
 
 
 //---------------------------------------------------------------------------
 %include "src/OpenFOAM/fields/GeometricFields/GeometricField_vector_fvPatchField_volMesh.cxx"
-
 %include "src/OpenFOAM/fields/GeometricFields/GeometricField_scalar_fvsPatchField_surfaceMesh.cxx"
 
+
+//---------------------------------------------------------------------------
 %typemap( out ) Foam::surfaceScalarField
 {
     $result = SWIG_NewPointerObj( ( new $1_type( *&$1 ) ), $&1_descriptor, SWIG_POINTER_OWN |  0 );
 }
 
+
+//---------------------------------------------------------------------------
 %inline
 %{
     #include "linear.H"
