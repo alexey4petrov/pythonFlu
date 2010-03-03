3333cc2c84a574c3658bcb42940450c447309d12
asimurzin <asimurzin@gmail.com>
2010-03-03 13:26:03 +0000

    to include "finiteVolume director's"

--- src/OpenFOAM/containers/Lists/PtrList/PtrList_basicPsiThermo.cxx
+++ src/OpenFOAM/containers/Lists/PtrList/PtrList_basicPsiThermo.cxx
@@ -31,6 +31,11 @@
 
 
 //---------------------------------------------------------------------------
+//It is necessary to include "director's" classes above first's DIRECTOR_INCLUDE
+%include "src/finiteVolume/directors.hxx"
+
+
+//---------------------------------------------------------------------------
 %include "src/thermophysicalModels/basic/psiThermo/basicPsiThermo.cxx"
 
 %include "src/OpenFOAM/fields/tmp/autoPtr_basicPsiThermo.cxx"
