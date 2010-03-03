2a4c22021b35c02bc22de08673a005f906100bdc
asimurzin <asimurzin@gmail.com>
2010-03-03 13:40:36 +0000

    to include "finiteVolume" director's

--- src/OpenFOAM/containers/Lists/PtrList/PtrList_fvMesh.cxx
+++ src/OpenFOAM/containers/Lists/PtrList/PtrList_fvMesh.cxx
@@ -24,6 +24,11 @@
 
 
 //---------------------------------------------------------------------------
+//It is necessary to include "director's" classes above first's DIRECTOR_INCLUDE
+%include "src/finiteVolume/directors.hxx"
+
+
+//---------------------------------------------------------------------------
 %include "src/OpenFOAM/fields/tmp/autoPtr_fvMesh.cxx"
 
 %include "src/OpenFOAM/containers/Lists/PtrList/PtrList.cxx"
