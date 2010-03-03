9d1aa9a34370b763a117220b1f873fe5c36e90ed
asimurzin <asimurzin@gmail.com>
2010-03-03 13:45:16 +0000

    to include finiteVolume director's

--- src/finiteVolume/cfdTools/general/adjustPhi.cxx
+++ src/finiteVolume/cfdTools/general/adjustPhi.cxx
@@ -24,6 +24,11 @@
 
 
 //---------------------------------------------------------------------------
+//It is necessary to include "director's" classes above first's DIRECTOR_INCLUDE
+%include "src/finiteVolume/directors.hxx"
+
+
+//---------------------------------------------------------------------------
 %include "src/common.hxx"
 
 %include "src/finiteVolume/fields/surfaceFields/surfaceFields.cxx"
--- src/finiteVolume/cfdTools/general/bound.cxx
+++ src/finiteVolume/cfdTools/general/bound.cxx
@@ -24,6 +24,11 @@
 
 
 //---------------------------------------------------------------------------
+//It is necessary to include "director's" classes above first's DIRECTOR_INCLUDE
+%include "src/finiteVolume/directors.hxx"
+
+
+//---------------------------------------------------------------------------
 %include "src/OpenFOAM/dimensionedTypes/dimensionedScalar.cxx"
 %include "src/finiteVolume/fields/volFields/volFieldsFwd.hxx"
 
