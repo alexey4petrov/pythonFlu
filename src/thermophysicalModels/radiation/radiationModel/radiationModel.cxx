bf03ecdf36bdcbba3076cdc6578b47c091f34521
asimurzin <asimurzin@gmail.com>
2010-03-05 16:15:50 +0000

    radiationModel

--- src/thermophysicalModels/radiation/radiationModel/radiationModel.cxx
+++ src/thermophysicalModels/radiation/radiationModel/radiationModel.cxx
@@ -19,7 +19,7 @@
 
 
 //--------------------------------------------------------------------------
-#if ( __FOAM_VERSION__ < 010600 )
+#if ( __FOAM_VERSION__ < 010500 )
 %include "src/common.hxx"
 #define radiationModel_cxx
 #endif
