7c318232cefa435fe1674e94f96aebf3c8ab088f
asimurzin <asimurzin@gmail.com>
2010-05-04 09:14:38 +0000

    basicRhoThermo is wrapped

---
+++ src/thermophysicalModels/basic/rhoThermo/basicRhoThermo.cxx
@@ -0,0 +1,55 @@
+//  VulaSHAKA (Simultaneous Neutronic, Fuel Performance, Heat And Kinetics Analysis)
+//  Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  See https://vulashaka.svn.sourceforge.net/svnroot/vulashaka/pyfoam
+//
+//  Author : Alexey PETROV
+
+
+//---------------------------------------------------------------------------
+#if ( __FOAM_VERSION__ < 010600 )
+#define basicRhoThermo_cxx
+%include "src/common.hxx"
+#endif
+
+
+//---------------------------------------------------------------------------
+#ifndef basicRhoThermo_cxx
+#define basicRhoThermo_cxx
+
+
+//---------------------------------------------------------------------------
+// Keep on corresponding "director" includes at the top of SWIG defintion file
+
+%include "src/OpenFOAM/directors.hxx"
+
+%include "src/finiteVolume/directors.hxx"
+
+
+//---------------------------------------------------------------------------
+%include "src/thermophysicalModels/basic/basicThermo.cxx"
+
+//%include "src/OpenFOAM/db/runTimeSelection/runTimeSelectionTables.hxx"
+
+%{
+    #include "basicRhoThermo.H"
+%}
+
+%include "basicRhoThermo.H"
+
+
+//---------------------------------------------------------------------------
+#endif
