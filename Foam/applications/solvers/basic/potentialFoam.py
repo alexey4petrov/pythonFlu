87eb1b93d3a1e187b19f2feaa799cd5f60d72497
asimurzin <asimurzin@gmail.com>
2010-06-18 06:28:28 +0000

    to merge with dev ( to change running setRefCell function )

--- Foam/applications/solvers/basic/potentialFoam.py
+++ Foam/applications/solvers/basic/potentialFoam.py
@@ -64,7 +64,7 @@ def _createFields( runTime, mesh ):
     pRefCell = 0
     pRefValue = 0.0
     from Foam.finiteVolume import setRefCell
-    setRefCell( p, mesh.solutionDict().subDict( word( "SIMPLE" ) ), pRefCell, pRefValue )
+    pRefCell, pRefValue = setRefCell( p, mesh.solutionDict().subDict( word( "SIMPLE" ) ), pRefCell, pRefValue )
            
     return p, U, phi, pRefCell, pRefValue
 
