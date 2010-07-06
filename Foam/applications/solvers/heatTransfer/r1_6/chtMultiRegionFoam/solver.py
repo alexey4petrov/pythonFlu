a5128ffcbf5a09929dfdef49b687416abad3a978
asimurzin <asimurzin@gmail.com>
2010-07-06 01:36:11 +0000

    minor changes ( to change copyrigths )

--- Foam/applications/solvers/heatTransfer/r1_6/chtMultiRegionFoam/solver.py
+++ Foam/applications/solvers/heatTransfer/r1_6/chtMultiRegionFoam/solver.py
@@ -17,7 +17,7 @@
 ## You should have received a copy of the GNU General Public License
 ## along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ## 
-## See https://vulashaka.svn.sourceforge.net/svnroot/vulashaka/pyfoam
+## See https://vulashaka.svn.sourceforge.net/svnroot/vulashaka
 ##
 ## Author : Alexey PETROV
 ##
