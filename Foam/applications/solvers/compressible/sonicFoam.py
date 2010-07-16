1276db8a7fecd7d98cd465bd7c3a59fd8633a7d5
asimurzin <asimurzin@gmail.com>
2010-07-16 09:18:25 +0000

    merge with r661_porting1.7 r713-716 ( porting sonicFoam )

--- Foam/applications/solvers/compressible/sonicFoam.py
+++ Foam/applications/solvers/compressible/sonicFoam.py
@@ -59,7 +59,7 @@ if WM_PROJECT_VERSION() == "1.5":
 
 
 #--------------------------------------------------------------------------------------
-if WM_PROJECT_VERSION() >= "1.6":
+if WM_PROJECT_VERSION() == "1.6":
    if __name__ == "__main__" :
       argv = sys.argv
       if len( argv ) > 1 and argv[ 1 ] == "-test":
@@ -73,3 +73,24 @@ if WM_PROJECT_VERSION() >= "1.6":
       from Foam.applications.solvers.compressible.r1_6.sonicFoam import *
       pass
    pass
+
+
+#--------------------------------------------------------------------------------------
+if WM_PROJECT_VERSION() >= "1.7.0":
+   if __name__ == "__main__" :
+      argv = sys.argv
+      if len( argv ) > 1 and argv[ 1 ] == "-test":
+         argv = None
+         test_dir= os.path.join( os.environ[ "PYFOAM_TESTING_DIR" ],'cases', 'r1.7.0', 'compressible', 'sonicFoam', 'ras', 'prism' )
+         argv = [ __file__, "-case", test_dir ]
+         pass
+      from Foam.applications.solvers.compressible.r1_6.sonicFoam import main_standalone
+      os._exit( main_standalone( len( argv ), argv ) )
+   else:
+      from Foam.applications.solvers.compressible.r1_6.sonicFoam import *
+      pass
+   pass
+
+
+#--------------------------------------------------------------------------------------
+
