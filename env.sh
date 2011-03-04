93b031712d4c483c7f09cce2ef9ca3fa88e607a5
alexey4petrov <alexey4petrov@yandex.ru>
2011-03-04 17:02:07 +0000

    rollback the wrong commit

--- env.sh
+++ env.sh
@@ -1,2 +1,44 @@
-source /home/alexey/confflu-deb-1.7.1-1/bashrc
-source_openfoam /opt/openfoam171/etc/bashrc
+## VulaSHAKA (Simultaneous Neutronic, Fuel Performance, Heat And Kinetics Analysis)
+## Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+## 
+## This program is free software: you can redistribute it and/or modify
+## it under the terms of the GNU General Public License as published by
+## the Free Software Foundation, either version 3 of the License, or
+## (at your option) any later version.
+##
+## This program is distributed in the hope that it will be useful,
+## but WITHOUT ANY WARRANTY; without even the implied warranty of
+## MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+## GNU General Public License for more details.
+## 
+## You should have received a copy of the GNU General Public License
+## along with this program.  If not, see <http://www.gnu.org/licenses/>.
+## 
+## See https://vulashaka.svn.sourceforge.net/svnroot/vulashaka
+##
+## Author : Alexey PETROV
+##
+
+
+#--------------------------------------------------------------------------------------
+# Source the conffoam environment
+source /opt/conffoam/r0.2/bashrc
+
+#--------------------------------------------------------------------------------------
+# Source the OpenFOAM environment
+which_open_foam="1.4.1-dev"
+source_openfoam /opt/OpenFOAM/r${which_open_foam}/debug/bashrc
+
+if [ "${which_open_foam}" == "1.4.1-dev" ]; then
+    source /opt/diffusionFoam/r1.4/OpenFOAM-${which_open_foam}/bashrc 
+fi
+
+# Source the SALOME environment
+source_salome /opt/SALOME/r5.1.3/r1.0/from_sources/env_products.sh
+
+# Source the VulaSHAKA utility components : unv2foam and foam2vtk
+source /opt/unv2foam/r0.4/OpenFOAM-${which_open_foam}/bashrc 
+source /opt/foam2vtk/r0.4/OpenFOAM-${which_open_foam}/bashrc 
+
+
+#--------------------------------------------------------------------------------------
