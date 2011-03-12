598f2d07225370669606fa93a5f075efceacd1ce
asimurzin <asimurzin@gmail.com>
2011-03-12 13:46:50 +0000

    to correct inaccuracy

--- Foam/applications/solvers/basic/r1_6/potentialFoam.py
+++ Foam/applications/solvers/basic/r1_6/potentialFoam.py
@@ -165,8 +165,8 @@ def main_standalone( argc, argv ):
 
 #--------------------------------------------------------------------------------------
 import sys, os
-from Foam import FOAM_VERSION
-if FOAM_VERSION( "==", "010600" ):
+from Foam import FOAM_REF_VERSION
+if FOAM_REF_VERSION( "==", "010600" ):
    if __name__ == "__main__" :
       argv = sys.argv
       if len( argv ) > 1 and argv[ 1 ] == "-test":
