228c5e1526a708a4c44757256df075b7e727e2b4
Andrey Simurzin <asimurzin@gmail.com>
2011-05-13 18:41:07 +0400

    to wrap HashTable<curve*>

---
+++ Foam/src/OpenFOAM/containers/HashTables/HashTable/HashTable_curve_word_string_hash.cxx
@@ -0,0 +1,55 @@
+//  VulaSHAKA (Simultaneous Neutronic, Fuel Performance, Heat And Kinetics Analysis)
+//  Copyright (C) 2009-2010 Pebble Bed Modular Reactor (Pty) Limited (PBMR)
+//  
+//  This program is free software: you can redistribute it and/or modify
+//  it under the terms of the GNU General Public License as published by
+//  the Free Software Foundation, either version 3 of the License, or
+//  (at your option) any later version.
+//  
+//  This program is distributed in the hope that it will be useful,
+//  but WITHOUT ANY WARRANTY; without even the implied warranty of
+//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//  GNU General Public License for more details.
+//  
+//  You should have received a copy of the GNU General Public License
+//  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+//
+//  See https://vulashaka.svn.sourceforge.net/svnroot/vulashaka
+//
+//  Author : Alexey PETROV
+
+
+//---------------------------------------------------------------------------
+#ifndef HashTable_curve_word_string_hash_cxx
+#define HashTable_curve_word_string_hash_cxx
+
+
+//---------------------------------------------------------------------------
+%module "Foam.src.OpenFOAM.containers.HashTables.HashTable.HashTable_curve_word_string_hash";
+%{
+    #include "src/OpenFOAM/containers/HashTables/HashTable/HashTable_curve_word_string_hash.hh"
+%}
+
+
+//---------------------------------------------------------------------------
+%import "src/OpenFOAM/containers/HashTables/HashTable/HashTable.cxx"
+
+%import "src/OpenFOAM/graph/curve.cxx"
+
+%ignore Foam::HashTable< Foam::curve*, Foam::word, Foam::string_hash >::begin;
+%ignore Foam::HashTable< Foam::curve*, Foam::word, Foam::string_hash >::find;
+%ignore Foam::HashTable< Foam::curve*, Foam::word, Foam::string_hash >::HashTable;
+
+#if FOAM_VERSION( >=, 010600 )
+  %ignore Foam::HashTable< Foam::curve*, Foam::word, Foam::string_hash >::cbegin;
+#endif
+
+%template( HashTable_curve_word_string_hash ) Foam::HashTable< Foam::curve*, Foam::word, Foam::string_hash >; 
+
+%template( TContainer_word_curve ) Foam::TContainer_iterator< Foam::HashTable< Foam::curve*, Foam::word, Foam::string_hash > >;
+
+HASHTABLE_ADDONS( Foam::curve*, Foam::word, Foam::string_hash );
+
+
+//---------------------------------------------------------------------------
+#endif
