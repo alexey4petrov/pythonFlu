627c1b1c71e587102c2afcf549e450c73b3247f0
Bernhard F.W. Gschaider <bgschaid@ice-sf.at>
2011-08-24 01:18:02 +0200

    Tried to move the function to a different location, but it doesn't work
    --HG--
    branch : bgschaid/swakFunctionObjectBranch
    rename : Foam/applications/Makefile => Foam/integrationHelpers/Makefile
    rename : Foam/applications/__init__.py => Foam/integrationHelpers/__init__.py
    rename : Foam/src/OpenFOAM/db/Time/Time.hh => Foam/integrationHelpers/getObjectsFromPointers.hh

---
+++ Foam/integrationHelpers/getObjectsFromPointers.cxx
@@ -0,0 +1,27 @@
+
+#ifndef getObjectsFromPointers_cxx
+#define getObjectsFromPointers_cxx
+
+%module "Foam.integrationHelpers.getObjectsFromPointers";
+%{
+    #include "Foam/integrationHelpers/getObjectsFromPointers.hh"
+%}
+
+%import "Foam/src/OpenFOAM/db/Time/Time.cxx"
+
+// %include <Time.H>
+
+%inline %{
+    Foam::Time &getTimeFromPtr(PyObject *c) {
+        if(!PyCObject_Check(c)) {
+            throw Swig::DirectorMethodException();
+        }
+        void *ptr=PyCObject_AsVoidPtr(c);
+//         if(!isA<Time>(*ptr)) {
+//             throw Swig::DirectorMethodException();
+//         }
+        return *static_cast<Foam::Time *>(ptr);
+    }
+%}
+
+#endif
