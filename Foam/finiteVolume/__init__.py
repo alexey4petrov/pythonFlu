54854dd1e049a4b3c6ec04d656630736bdef8f22
Andrey Simurzin <asimurzin@gmail.com>
2011-09-14 09:43:47 +0400

    to add volScalarFieldHolder

--- Foam/finiteVolume/__init__.py
+++ Foam/finiteVolume/__init__.py
@@ -87,6 +87,16 @@ surfaceVectorField = GeometricField_vector_fvsPatchField_surfaceMesh
 
 
 #---------------------------------------------------------------------------
+volScalarFieldHolder = GeometricFieldHolder_scalar_fvPatchField_volMesh
+
+volVectorFieldHolder = GeometricFieldHolder_vector_fvPatchField_volMesh
+
+surfaceScalarFieldHolder = GeometricFieldHolder_scalar_fvsPatchField_surfaceMesh
+
+surfaceVectorFieldHolder = GeometricFieldHolder_vector_fvsPatchField_surfaceMesh
+
+
+#---------------------------------------------------------------------------
 fvPatchScalarField = fvPatchField_scalar
 
 fvPatchVectorField = fvPatchField_vector
