cb820faab0c794efa19cfbbf7d106e60fa1a8107
Andrey Simurzin <asimurzin@gmail.com>
2011-11-10 09:16:34 +0300

    porting to 2.0.0

--- Foam/finiteVolume/cfdTools/general/include/__init__.py
+++ Foam/finiteVolume/cfdTools/general/include/__init__.py
@@ -61,18 +61,12 @@ def readTimeControls( runTime ):
     
 
 #----------------------------------------------------------------------------
-def setInitialDeltaT( runTime, adjustTimeStep, maxCo, maxDeltaT, CoNum ):
-    from Foam.OpenFOAM import SMALL 
-
-    if adjustTimeStep :
-        if runTime.timeIndex() == 0:
-            if CoNum > SMALL :
-                runTime.setDeltaT( min( maxCo * runTime.deltaT().value() / CoNum, maxDeltaT ) )
-                pass
-            pass
-	pass
+def setInitialDeltaT( *args ):
+    from Foam import get_proper_function
+    fun = get_proper_function( "Foam.finiteVolume.cfdTools.general.include.setInitialDeltaT_impl",
+                               "setInitialDeltaT" )
     
-    return runTime         
+    return fun( *args )        
 
 
 #------------------------------------------------------------------------------------
