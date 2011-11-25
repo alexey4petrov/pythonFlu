87a69b10fc727685f46b194313c569aeb4e722ec
Andrey Simurzin <asimurzin@gmail.com>
2011-11-25 11:52:46 +0300

    using ref feature

--- unittest/testContainers/testList.py
+++ unittest/testContainers/testList.py
@@ -1,17 +1,17 @@
-from Foam.OpenFOAM import scalarList
+from Foam import ref
 from nose.tools import ok_,eq_,nottest,raises
 
 def testListCreation():
-    l=scalarList(4,1.)
+    l=ref.scalarList(4,1.)
     eq_(len(l),4)
     eq_(l[2],1.)
     
 @raises(NotImplementedError)
 def testListCreationFails():
-    l=scalarList(4,1)
+    l=ref.scalarList(4,1)
 
 @raises(TypeError)
 def testListIteration():
-    l=scalarList(4,1.)
+    l=ref.scalarList(4,1.)
     for i in l:
         eq_(i,1.)
