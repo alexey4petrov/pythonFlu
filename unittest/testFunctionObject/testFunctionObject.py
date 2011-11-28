31766487bd13e5d31b7cba6a09e74771c441178f
Andrey Simurzin <asimurzin@gmail.com>
2011-11-28 13:36:30 +0300

    to add test case for functionObject

---
+++ unittest/testFunctionObject/testFunctionObject.py
@@ -0,0 +1,18 @@
+import Foam.functionObjects
+from Foam import ref, man
+
+import unittest
+
+
+#------------------------------------------------------------------------------------
+class testFunctionObject(unittest.TestCase):
+    def setUp(self):
+        import os
+        argv=["test","-case",os.path.join( os.environ[ "PYTHONFLU_ROOT_DIR" ], "unittest/testFunctionObject" ) ] 
+        args = ref.setRootCase( len(argv), argv )
+        self.runTime = man.createTime( args )
+
+    def testCycle(self):
+        while self.runTime.loop():
+            pass
+
